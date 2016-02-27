8a6d71e94762b50a22aed05f0381e095085cd715
Gerik Kubiak <gerikkub@gmail.com>
2016-02-27 10:08:12 -0800

    Fixed rebase issue with the usart driver

--- generatedPerif/usart.c
+++ generatedPerif/usart.c
@@ -69,7 +69,6 @@ void MX_USART1_UART_Init(void)
   huart1.Init.StopBits = UART_STOPBITS_1;
   huart1.Init.Parity = UART_PARITY_NONE;
   huart1.Init.Mode = UART_MODE_TX_RX;
-  huart1.Init.Mode = UART_MODE_RX;
   huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
   huart1.Init.OverSampling = UART_OVERSAMPLING_16;
   HAL_UART_Init(&huart1);
