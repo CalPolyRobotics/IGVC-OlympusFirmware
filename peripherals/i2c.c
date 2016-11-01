1250c40c15e5b45d037cda7195b4eed626b6ff51
Gerik Kubiak <gerikkub@gmail.com>
2016-11-01 13:14:53 -0700

    Changes to i2c peripheral

--- peripherals/i2c.c
+++ peripherals/i2c.c
@@ -6,6 +6,7 @@
 #include "i2c.h"
 #include "gpio.h"
 #include "buffer8.h"
+#include "usart.h"
 
 typedef enum {
     I2C_TYPE_TX,
@@ -29,6 +30,8 @@ static uint8_t i2cTxDataBuffer[I2C_TRANSACTION_TX_BUF_SIZE];
 static uint8_t i2cRxDataBuffer[I2C_TRANSACTION_RX_BUF_SIZE];
 static uint32_t i2cRxDataBufferIdx;
 static buffer8_t i2cTxDataFifo;
+static uint8_t i2cTxNextTransferBuffer[I2C_TRANSACTION_TX_BUF_SIZE];
+static uint32_t i2cTxNextTransferIdx;
 
 static uint32_t freeTransaction;
 static volatile uint32_t nextTransaction;
@@ -75,10 +78,18 @@ static void startI2CTransfer()
 
     i2cBytesLeft = transactionList[nextTransaction].numBytes;
 
-    if (transactionList[nextTransaction].type == I2C_TYPE_RX)
+    if (transactionList[nextTransaction].type == I2C_TYPE_TX)
     {
+        uint32_t i;
+        for (i = 0; i < transactionList[nextTransaction].numBytes; i++)
+        {
+            i2cTxNextTransferBuffer[i] = buffer8_get(&i2cTxDataFifo);
+        }
+        i2cTxNextTransferIdx = 0;
+    } else {
         I2C2->CR1 |= I2C_CR1_ACK;
     }
+
     
     I2C2->CR1 |= I2C_CR1_START;
 }
@@ -141,90 +152,80 @@ uint8_t i2cAddTxTransaction(uint8_t addr, uint8_t* txData, uint32_t numBytes, i2
     return true;
 }
 
+static volatile uint32_t lastIRQState;
+
 void serviceI2C()
 {
+    uint8_t lastTransaction;
+    I2CStatus lastStatus;
+
     if (shouldServiceI2C)
     {
-        shouldServiceI2C = 0;
-        i2cRunning = 0;
-        transactionList[nextTransaction].ready = 0;
-
-        while (I2C2->SR2 & I2C_SR2_BUSY);
-        I2C2->CR1 &= ~(I2C_CR1_STOP);
-
-        // Transaction has completed
-
-        if (i2cStatus != I2C_ACK)
+        if(!(I2C2->SR2 & I2C_SR2_BUSY))
         {
-            // Transaction failed
-            if (transactionList[nextTransaction].type == I2C_TYPE_TX)
-            {
-                // Clear the remaining bytes in the fifo
-                while (i2cBytesLeft > 0)
-                {
-                    buffer8_get(&i2cTxDataFifo);
-                    i2cBytesLeft--;
-                }
+            I2C2->CR1 &= ~(I2C_CR1_STOP);
 
-                if (transactionList[nextTransaction].txCallback)
+            shouldServiceI2C = 0;
+            lastTransaction = nextTransaction;
+            lastStatus = i2cStatus;
+
+            // Transaction has completed
+
+            if (i2cStatus != I2C_ACK)
+            {
+                // Transaction failed
+                if (transactionList[nextTransaction].type == I2C_TYPE_TX)
                 {
-                    transactionList[nextTransaction].txCallback(
-                            transactionList[nextTransaction].cParams,
-                            i2cStatus);
                 }
             } else {
-                if (transactionList[nextTransaction].rxCallback)
-                {
-                    transactionList[nextTransaction].rxCallback(
-                            transactionList[nextTransaction].cParams,
-                            NULL,
-                            0,
-                            i2cStatus);
-                }
-            }
-        } else {
-            // Transaction successful
-           
-            if (transactionList[nextTransaction].type == I2C_TYPE_TX)
-            {
-                if (transactionList[nextTransaction].txCallback)
-                {
-                    transactionList[nextTransaction].txCallback(
-                            transactionList[nextTransaction].cParams,
-                            i2cStatus);
-                }
-            } else {
-
+                // Transaction successful
                 if (transactionList[nextTransaction].numBytes == 1)
                 {
                     i2cRxDataBuffer[i2cRxDataBufferIdx] = I2C2->DR;
                     i2cRxDataBufferIdx++;
                 }
+            }
 
-                if (transactionList[nextTransaction].rxCallback)
+
+
+
+            // Reset I2C for the next transaction
+            i2cRxDataBufferIdx = 0;
+            i2cStatus = I2C_ACK;
+
+            nextTransaction++;
+            if (nextTransaction == I2C_MAX_NUM_TRANSACTIONS)
+            {
+                nextTransaction = 0;
+            }
+
+            if (transactionList[lastTransaction].type == I2C_TYPE_TX)
+            {
+                if (transactionList[lastTransaction].txCallback)
                 {
-                    transactionList[nextTransaction].rxCallback(
-                            transactionList[nextTransaction].cParams,
+                    transactionList[lastTransaction].txCallback(
+                            transactionList[lastTransaction].cParams,
+                            lastStatus);
+                }
+            } else {
+
+                if (transactionList[lastTransaction].rxCallback)
+                {
+                    transactionList[lastTransaction].rxCallback(
+                            transactionList[lastTransaction].cParams,
                             i2cRxDataBuffer,
                             i2cRxDataBufferIdx,
-                            i2cStatus);
+                            lastStatus);
                 }
             }
-        }
 
-        // Reset I2C for the next transaction
-        i2cRxDataBufferIdx = 0;
-        i2cStatus = I2C_ACK;
+            transactionList[lastTransaction].ready = 0;
+            i2cRunning = 0;
 
-        nextTransaction++;
-        if (nextTransaction == I2C_MAX_NUM_TRANSACTIONS)
-        {
-            nextTransaction = 0;
-        }
-
-        if (transactionList[nextTransaction].ready == 1)
-        {
-            startI2CTransfer();
+            if (transactionList[nextTransaction].ready == 1)
+            {
+                startI2CTransfer();
+            }
         }
     }
 }
@@ -249,6 +250,7 @@ static void readI2CByte()
 
 }
 
+
 void I2C2_EV_IRQHandler()
 {
     uint32_t i2cStatusReg = I2C2->SR1;
@@ -260,9 +262,11 @@ void I2C2_EV_IRQHandler()
         {
             // Send address with write
             I2C2->DR = transactionList[nextTransaction].addr << 1 | 0;
+            lastIRQState = 1;
         } else {
             // Send address with read
             I2C2->DR = transactionList[nextTransaction].addr << 1 | 1;
+            lastIRQState = 2;
         }
     }
 
@@ -276,11 +280,14 @@ void I2C2_EV_IRQHandler()
         {
             if (i2cBytesLeft > 0)
             {
-                I2C2->DR = buffer8_get(&i2cTxDataFifo);
+                I2C2->DR = i2cTxNextTransferBuffer[i2cTxNextTransferIdx++];
                 i2cBytesLeft--;
+                lastIRQState = 3;
             } else {
                 I2C2->CR1 |= I2C_CR1_STOP;
                 shouldServiceI2C = 1;
+
+                lastIRQState = 4;
             }
         } else {
 
@@ -290,10 +297,18 @@ void I2C2_EV_IRQHandler()
                 I2C2->CR1 &= ~I2C_CR1_ACK;
                 I2C2->CR1 |= I2C_CR1_STOP;
                 shouldServiceI2C = 1;
+
+
+                lastIRQState = 5;
             } else if (i2cBytesLeft == 2)
             {
                 I2C2->CR1 &= ~I2C_CR1_ACK;
                 I2C2->CR1 |= I2C_CR1_POS;
+
+
+                lastIRQState = 6;
+            } else {
+                lastIRQState = 7;
             }
         }
     }
@@ -306,14 +321,17 @@ void I2C2_EV_IRQHandler()
         {
             if (i2cBytesLeft > 0)
             {
-                I2C2->DR = buffer8_get(&i2cTxDataFifo);
+                I2C2->DR = i2cTxNextTransferBuffer[i2cTxNextTransferIdx++];
                 i2cBytesLeft--;
+                lastIRQState = 8;
             } else {
                 I2C2->CR1 |= I2C_CR1_STOP;
                 shouldServiceI2C = 1;
+                lastIRQState = 9;
             }
         } else {
             readI2CByte();
+            lastIRQState = 10;
         }
     }
 }
@@ -328,6 +346,7 @@ void I2C2_ER_IRQHandler()
         shouldServiceI2C = 1;
         I2C2->SR1 &= ~(I2C_SR1_AF);
         i2cStatus = I2C_NACK;
+        lastIRQState = 11;
     }
 
     // Arbitration Lost
@@ -336,6 +355,7 @@ void I2C2_ER_IRQHandler()
         shouldServiceI2C = 1;
         I2C2->SR1 &= ~(I2C_SR1_ARLO);
         i2cStatus = I2C_ERR;
+        lastIRQState = 12;
     }
 
     // Bus Error
@@ -344,6 +364,7 @@ void I2C2_ER_IRQHandler()
         shouldServiceI2C = 1;
         I2C2->SR1 &= ~(I2C_SR1_BERR);
         i2cStatus = I2C_ERR;
+        lastIRQState = 13;
     }
 
     I2C2->CR1 |= I2C_CR1_STOP;
