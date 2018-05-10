4c084c609baa1b73815516c9a2344abf7d976440
Kyle Wuerch <kwuerch@calpoly.edu>
2018-05-09 19:11:25 -0700

    Create project structure and comms structure for Hephaestus

---
+++ M0/hephaestus/drivers/comms.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "comms.h"
+
+uint8_t *bootload(uint8_t* data);
+
+/** tx or rx DataLengths can be no longer than 253(rx) 253(tx) **/
+msgInfo_t msgResp[NUM_MSGS] = {
+    {0, 0, NULL}, /** 0 Unused **/
+    {0, 0, NULL}, /** 1 Unused **/
+    {0, 0, NULL}, /** 2 Unused **/
+    {0, 0, NULL}, /** 3 Unused **/
+    {0, 0, NULL}, /** 4 Unused **/
+    {0, 0, NULL}, /** 5 Unused **/
+    {0, 0, NULL}, /** 6 Unused **/
+    {0, 0, NULL}, /** 7 Unused **/
+    {0, 0, NULL}, /** 8 Unused **/
+    {0, 0, NULL}, /** 9 Unused **/
+    {0, 0, bootload}, /** 10 Reset To Bootloader **/
+};
