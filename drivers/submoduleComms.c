2b762023d3f12126b70f76dd0e6623e67c0790c7
Kyle Wuerch <kyle98678@gmail.com>
2018-05-12 13:03:57 -0700

    Update submodule comms CS pin LUT

--- drivers/submoduleComms.c
+++ drivers/submoduleComms.c
@@ -11,16 +11,11 @@ static void deselectModule(module_t module);
 
 uint8_t submoduleCommsBuff[256];
 
-typedef struct pin{
-    GPIO_TypeDef* port;
-    uint16_t pin_mask;
-}pin_t;
-
 static pin_t CS_PINS[NUM_SUBMODULES] = {
-    {GPIOA, GPIO_PIN_4}, /* APOLLO CS PIN */
-    {GPIOA, GPIO_PIN_4}, /* TODO: HEPHAESTUS CS PIN */
-    {GPIOA, GPIO_PIN_4}, /* TODO: HERA CS PIN */
-    {GPIOA, GPIO_PIN_4}, /* TODO: JANUS CS PIN */
+    {PORT_SS_APOLLO, PIN_SS_APOLLO},
+    {PORT_SS_HEPHAESTUS, PIN_SS_HEPHAESTUS},
+    {PORT_SS_HERA, PIN_SS_HERA},
+    {PORT_SS_JANUS, PIN_SS_JANUS},
 };
 
 void messageSubmodule(module_t module, uint8_t msg_type, uint8_t* buff, uint8_t tx_size, uint8_t rx_size)
