10b639c49344fb5698ab67d17679eb1c50a86698
Kyle Wuerch <kyle98678@gmail.com>
2018-05-12 12:52:44 -0700

    Add status message to slave MCUs

--- M0/hephaestus/drivers/comms.c
+++ M0/hephaestus/drivers/comms.c
@@ -1,11 +1,14 @@
 #include <stdlib.h>
+#include "boot.h"
 #include "comms.h"
 
-uint8_t *bootload(uint8_t* data);
+static uint8_t errorByte;
+
+static uint8_t *get_status_callback(uint8_t *data);
 
 /** tx or rx DataLengths can be no longer than 253(rx) 253(tx) **/
 msgInfo_t msgResp[NUM_MSGS] = {
-    {0, 0, NULL}, /** 0 Unused **/
+    {0, 1, get_status_callback}, /** 0 Get Status **/
     {0, 0, NULL}, /** 1 Unused **/
     {0, 0, NULL}, /** 2 Unused **/
     {0, 0, NULL}, /** 3 Unused **/
@@ -17,3 +20,8 @@ msgInfo_t msgResp[NUM_MSGS] = {
     {0, 0, NULL}, /** 9 Unused **/
     {0, 0, bootload}, /** 10 Reset To Bootloader **/
 };
+
+static uint8_t *get_status_callback(uint8_t *data){
+    errorByte = WR_OK;
+    return &errorByte;
+}
