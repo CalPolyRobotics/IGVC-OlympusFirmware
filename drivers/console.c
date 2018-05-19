c5e5d6f2c2c94d232d19e1e3cbd8320276299ef6
Kyle Wuerch <kyle98678@gmail.com>
2018-05-19 11:22:38 -0700

    Refactor console.c

--- drivers/console.c
+++ drivers/console.c
@@ -6,7 +6,6 @@
 #include "main.h"
 #include "usart.h"
 #include "led.h"
-#include "i2c.h"
 #include "pwradc.h"
 #include "sevenSeg.h"
 #include "speedDAC.h"
@@ -27,58 +26,69 @@ typedef struct {
     commandCallback callback;
 } ConsoleCommand;
 
-typedef enum {CONSOLE_START = 0, CONSOLE_READING} ConsoleState;
+typedef enum {
+    CONSOLE_START = 0,
+    CONSOLE_READING
+}ConsoleState;
 
-typedef enum {CONSOLE_ECHO_ON = 0, CONSOLE_ECHO_OFF, CONSOLE_ECHO_MAP, CONSOLE_ECHO_DEBUG} EchoState;
+typedef enum {
+    CONSOLE_ECHO_ON = 0,
+    CONSOLE_ECHO_OFF,
+    CONSOLE_ECHO_MAP,
+    CONSOLE_ECHO_DEBUG
+}EchoState;
 
-static void console_i2cWrite(uint32_t, char**);
-//static void console_i2cRead(uint32_t, char**);
-static void console_i2cScan(uint32_t, char**);
-//static void console_setGPIO(uint32_t, char**);
+
+// Board Commands
 static void console_setLED(uint32_t, char**);
 static void console_setSegment(uint32_t, char**);
-static void console_measPower(uint32_t, char**);
+static void console_getPower(uint32_t, char**);
 static void console_kill(uint32_t, char**);
-static void console_writeFNR(uint32_t, char**);
-static void console_readFNR(uint32_t, char**);
-static void console_writeSpeed(uint32_t, char**);
-static void console_readSpeed(uint32_t, char**);
+
+// FNR Commands
+static void console_setFNR(uint32_t, char**);
+static void console_getFNR(uint32_t, char**);
+
+// Speed Commands
 static void console_setEnableSpeed(uint32_t, char**);
-static void console_emulateUSB(uint32_t, char**);
-static void console_setSteerAngle(uint32_t, char**);
-static void console_USBWrite(uint32_t, char**);
-static void console_readSteeringDir(uint32_t, char**);
-static void console_setRawSteerAngle(uint32_t, char**);
+static void console_setSpeedTarget(uint32_t, char**);
+static void console_getSpeedTarget(uint32_t, char**);
+
+// Steering Commands
+static void console_setEnableSteer(uint32_t, char**);
+static void console_setSteerTarget(uint32_t, char**);
+static void console_getSteerDir(uint32_t, char**);
 static void console_getSteerTarget(uint32_t, char**);
-static void console_getSteerValue(uint32_t, char**);
-static void console_enableSteering(uint32_t, char**);
-static void console_hardmode(uint32_t, char**);
+static void console_getSteerPot(uint32_t, char**);
+
+// Communication Commands
+static void console_emulateUSB(uint32_t, char**);
+static void console_USBWrite(uint32_t, char**);
+
+// Console Commands
 static void console_help(uint32_t, char**);
 static void console_clear(uint32_t, char**);
+static void console_setEchoMode(uint32_t, char**);
+
 
 static ConsoleCommand commands[] = {
-    {"i2cWrite", 2, console_i2cWrite},
-    //{"i2cRead", 2, console_i2cRead},
-    {"i2cScan", 0, console_i2cScan},
-    //{"setGPIO", 3, console_setGPIO},
     {"setLED", 2, console_setLED},
     {"setSegment", 2, console_setSegment},
-    {"measPower", 1, console_measPower},
+    {"getPower", 1, console_getPower},
     {"kill", 0, console_kill},
-    {"writeFNR", 1, console_writeFNR},
-    {"readFNR", 0, console_readFNR},
-    {"writeSpeed", 1, console_writeSpeed},
-    {"readSpeed", 1, console_readSpeed},
+    {"setFNR", 1, console_setFNR},
+    {"getFNR", 0, console_getFNR},
     {"setEnableSpeed", 0, console_setEnableSpeed},
-    {"readSteeringDir", 0, console_readSteeringDir},
-    {"emulateUSB", 1, console_emulateUSB},
-    {"setSteerAngle", 2, console_setSteerAngle},
-    {"setRawSteerAngle", 1, console_setRawSteerAngle},
-    {"USBWrite", 1, console_USBWrite},
+    {"setSpeedTarget", 1, console_setSpeedTarget},
+    {"getSpeedTarget", 1, console_getSpeedTarget},
+    {"setEnableSteer", 1, console_setEnableSteer},
+    {"setSteerTarget", 1, console_setSteerTarget},
+    {"getSteerDir", 0, console_getSteerDir},
     {"getSteerTarget", 0, console_getSteerTarget},
-    {"getSteerValue", 0, console_getSteerValue},
-    {"enableSteering", 1, console_enableSteering},
-    {"hardmode", 1, console_hardmode},
+    {"getSteerPot", 0, console_getSteerPot},
+    {"emulateUSB", 1, console_emulateUSB},
+    {"USBWrite", 1, console_USBWrite},
+    {"setEchoMode", 1, console_setEchoMode},
     {"help", 0, console_help},
     {"clear", 0, console_clear},
     {NULL, 0, NULL}
@@ -328,41 +338,6 @@ void consoleProcessBytes()
     }
 }
 
-static void console_i2cWrite(uint32_t argc, char** argv)
-{
-    //uint8_t addr;
-    //uint8_t dataBytes[CONSOLE_MAX_NUM_ARGS];
-    //uint32_t i;
-
-    //addr = atoi(argv[0]);
-
-    //for (i = 1; i < argc; i++)
-    //{
-        //dataBytes[i-1] = atoi(argv[i]);
-    //}
-
-    //i2cTransmit(addr, dataBytes, argc-1);
-}
-
-/*
-static void console_i2cRead(uint32_t argc, char** argv) 
-{
-
-}
-*/
-
-static void console_i2cScan(uint32_t argc, char** argv)
-{
-    //i2cScan();
-}
-
-/*
-static void console_setGPIO(uint32_t argc, char** argv)
-{
-
-}
-*/
-
 static void console_setLED(uint32_t argc, char** argv)
 {
     uint8_t ledNum = atoi(argv[0]);
@@ -378,20 +353,9 @@ static void console_setLED(uint32_t argc, char** argv)
     }
 }
 
-static char segmentCharFromChar(char c){
-    if (c >= '0' && c <= '9')
-    {
-        return c - '0';
-    } else if(c >= 'a' && c <= 'f'){
-        return c - 'a' + 0xA;
-    } else {
-        return c;
-    }
-}
-
 static void console_setSegment(uint32_t argc, char** argv)
 {
-    setSevenSeg(segmentCharFromChar(*argv[0]), segmentCharFromChar(*argv[1]));
+    setSevenSeg(*argv[0], *argv[1]);
 }
 
 struct adc_cmd {
@@ -411,7 +375,7 @@ static const struct adc_cmd adc_dict[] = {
     {0}
 };
 
-static void console_measPower(uint32_t argc, char** argv)
+static void console_getPower(uint32_t argc, char** argv)
 {
     const struct adc_cmd *val;
 
@@ -428,28 +392,14 @@ static void console_kill(uint32_t argc, char** argv)
     killBoard();
 }
 
-static void console_writeFNR(uint32_t argc, char** argv)
+static void console_setFNR(uint32_t argc, char** argv)
 {
     /** TODO **/
 }
 
-static void console_readFNR(uint32_t argc, char** argv)
+static void console_getFNR(uint32_t argc, char** argv)
 {
     /** TODO **/
-}
-
-static void console_writeSpeed(uint32_t argc, char** argv)
-{
-    uint16_t num = parseUint16(argv[0]);
-    if(!errno){
-        writeSpeedDAC(num);
-    }
-}
-
-
-static void console_readSpeed(uint32_t argc, char** argv)
-{
-    printf("%u\r\n", getSpeedDAC());
 }
 
 /**
@@ -465,6 +415,45 @@ static void console_setEnableSpeed(uint32_t argc, char** argv){
             disableSpeedDAC();
         }
     }
+}
+
+static void console_setSpeedTarget(uint32_t argc, char** argv)
+{
+    uint16_t num = parseUint16(argv[0]);
+    if(!errno){
+        writeSpeedDAC(num);
+    }
+}
+
+
+static void console_getSpeedTarget(uint32_t argc, char** argv)
+{
+    printf("%u\r\n", getSpeedDAC());
+}
+
+static void console_setEnableSteer(uint32_t argc, char** argv)
+{
+    /** TODO **/
+}
+
+static void console_setSteerTarget(uint32_t argc, char** argv)
+{
+    /** TODO **/
+}
+
+static void console_getSteerDir(uint32_t argc, char** argv)
+{
+    /** TODO **/
+}
+
+static void console_getSteerTarget(uint32_t argc, char** argv)
+{
+    /** TODO **/
+}
+
+static void console_getSteerPot(uint32_t argc, char** argv)
+{
+    /** TODO **/
 }
 
 static void console_emulateUSB(uint32_t argc, char** argv)
@@ -491,43 +480,13 @@ static void console_emulateUSB(uint32_t argc, char** argv)
     }
 }
 
-static void console_setSteerAngle(uint32_t argc, char** argv)
-{
-    /** TODO **/
-}
-
-static void console_setRawSteerAngle(uint32_t argc, char** argv)
-{
-    /** TODO **/
-}
-
-static void console_getSteerTarget(uint32_t argc, char** argv)
-{
-    /** TODO **/
-}
-
-static void console_getSteerValue(uint32_t argc, char** argv)
-{
-    /** TODO **/
-}
-
-static void console_readSteeringDir(uint32_t argc, char** argv)
-{
-    /** TODO **/
-}
-
 static void console_USBWrite(uint32_t argc, char** argv)
 {
     printf("%s", argv[0]);
     usbWrite((uint8_t*)argv[0], strlen(argv[0]));
 }
 
-static void console_enableSteering(uint32_t argc, char** argv)
-{
-    /** TODO **/
-}
-
-static void console_hardmode(uint32_t argc, char** argv)
+static void console_setEchoMode(uint32_t argc, char** argv)
 {
     echoCharacters = strtol(argv[0], NULL, 10);
 }
@@ -535,10 +494,10 @@ static void console_hardmode(uint32_t argc, char** argv)
 static void console_help(uint32_t argc, char** argv) 
 {
     uint32_t i = 0;
-    printf("***Command Summary***");
+    printf("***Command Summary***\r\n");
     while (commands[i].cmdStr != NULL) {
         printf("%s", commands[i].cmdStr);
-        printf("\t requires %lu arguments\n", commands[i].minArguments);
+        printf("\t requires %lu arguments\r\n", commands[i].minArguments);
         i++;
     }
 }
