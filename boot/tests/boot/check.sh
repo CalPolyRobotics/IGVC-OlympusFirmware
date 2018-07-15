4ec9c28072e9d07b2b6e1f8739d5ce8c1935d817
Kyle Wuerch <kyle98678@gmail.com>
2018-07-15 15:44:50 -0700

    Add binary comparison check for bootloader

---
+++ boot/tests/boot/check.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+usbFlasher/BootComms.py clearLights.bin
+st-flash read temp.bin 0x8020000 0xF790
+hexdump temp.bin > tempdump
+hexdump clearLights.bin > pgrmdump
+vimdiff tempdump pgrmdump
