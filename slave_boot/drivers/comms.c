85188c129e9432b9c416e3222afa7e9d9c551923
Kyle Wuerch <kyle98678@gmail.com>
2018-08-19 20:09:49 -0600

    Add flash-write functionality to slave bootloader

--- slave_boot/drivers/comms.c
+++ slave_boot/drivers/comms.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
+#include <stdbool.h>
 #include "boot.h"
 #include "comms.h"
+#include "flash.h"
 
 #define ERASE_FLASH_KEY ((uint32_t)0x666C6170)
 
@@ -11,6 +13,7 @@ typedef struct{
 
 static uint8_t errorByte;
 static uint32_t size;
+static bool flashComplete = false;
 
 static uint8_t *get_status_callback(uint8_t *data);
 static uint8_t *write_header(uint8_t *data);
@@ -46,17 +49,52 @@ static uint8_t *write_header(uint8_t *data){
         return &errorByte;
     }
 
+    if(header.size > APP_MAX_SIZE){
+        errorByte = WR_ERR;
+        return &errorByte;
+    }
+
     size = header.size;
+
+    /* Initialize Flash */
+    writeInit(header.size);
+
     errorByte = WR_OK;
     return &errorByte;
 }
 
 static uint8_t *write_data(uint8_t *data){
+    static uint32_t count = 0;
+    static uint16_t* addr = (uint16_t*)APP_START_ADDR;
+
+    if(size == 0){
+        errorByte = WR_ERR;
+        return &errorByte;
+    }
+
+    /* Write data 16 bits at a time */
+    int i;
+    for(i = 0; i < 64u; i+=2u){
+        uint16_t val = ((uint16_t)data[1 + i] << 8u)  | (uint16_t)data[0 + i];
+        writeFlash(addr++, val);
+    }
+    count += msgResp[2].rxDataLength;
+
+    if(count >= size){
+        writeComplete();
+        flashComplete = true;
+    }
+
     errorByte = WR_OK;
     return &errorByte;
 }
 
 static uint8_t *run_checksum(uint8_t *data){
+    if(!flashComplete){
+        errorByte = WR_ERR;
+        return &errorByte;
+    }
+
     errorByte = WR_OK;
     return &errorByte;
 }
---
+++ slave_boot/peripherals/flash.c
@@ -0,0 +1,70 @@
+#include "stm32f0xx.h"
+#include "flash.h"
+
+static void erasePage(uint8_t page);
+static void checkEOP();
+
+void writeInit(uint32_t progsize)
+{
+    /* Wait for FLASH to be ready */
+    //while (FLASH->SR & FLASH_SR_BSY)
+
+    /* Unlock FLASH registers */
+    FLASH->KEYR = FLASH_KEY1;
+    FLASH->KEYR = FLASH_KEY2;
+
+    uint32_t page;
+
+    /* Calculate the minimum number of pages to erase */
+    uint32_t numPages = (progsize / PAGE_SIZE) + (progsize % PAGE_SIZE ? 1 : 0);
+    for(page = 0; page < numPages; page++)
+    {
+        erasePage(APP_START_PAGE + page);
+    }
+
+}
+
+void writeFlash(uint16_t* addr, uint16_t data)
+{
+    /* Set Flash to write mode */
+    FLASH->CR |= FLASH_CR_PG;
+
+    /* Flash must be written 16 bits at a time */
+    *addr = data;
+    while(FLASH->SR & FLASH_SR_BSY);
+
+    checkEOP();
+
+    FLASH->CR &= ~FLASH_CR_PG;
+}
+
+void writeComplete()
+{
+    /* Disable flash programming */
+    FLASH->CR &= ~FLASH_CR_PG;
+}
+
+static void erasePage(uint8_t page){
+    while(FLASH -> SR & FLASH_SR_BSY);
+
+    /* Enable page erase */
+    FLASH->CR = FLASH_CR_PER;
+
+    /* Select page and start erase */
+    FLASH->AR = FLASH_BASE + (page * PAGE_SIZE);
+    FLASH->CR |= FLASH_CR_STRT;
+
+    while(FLASH -> SR & FLASH_SR_BSY);
+    checkEOP();
+
+    /* Disable page erase */
+    FLASH->CR &= ~FLASH_CR_PER;
+}
+
+static void checkEOP(){
+    if (FLASH->SR & FLASH_SR_EOP) /* (5) */
+    {
+        /* Check and clear EOP - TODO handle else case */
+        FLASH->SR = FLASH_SR_EOP; /* (6)*/
+    }
+}
