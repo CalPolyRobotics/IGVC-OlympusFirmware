ea4188a874934ceb5b52d4151f01963ae6e1b122
Nicholas Sheffler <nsheffle@calpoly.edu>
2019-01-13 17:22:51 -0800

    start of error catching

---
+++ olympus/drivers/error.h
@@ -0,0 +1,8 @@
+#ifndef __error_H
+#define __error_H
+
+#include "gpio.h"
+
+void assert_failed(uint8_t* file, uint32_t line);
+
+#endif
--- olympus/main.c
+++ olympus/main.c
@@ -22,6 +22,7 @@
 #include "doubleBuffer.h"
 #include "submoduleComms.h"
 #include "sounds.h"
+#include "error.h"
 
 void SystemClock_Config(void);
 
@@ -170,23 +171,6 @@ void SystemClock_Config(void)
     HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
 #endif
 }
-
-#ifdef USE_FULL_ASSERT
-
-/**
-   * @brief Reports the name of the source file and the source line number
-   * where the assert_param error has occurred.
-   * @param file: pointer to the source file name
-   * @param line: assert_param error line source number
-   * @retval None
-   */
-void assert_failed(uint8_t* file, uint32_t line)
-{
-    /* User can add his own implementation to report the file name and line number,
-    ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-}
-
-#endif
 
 /**
   * @}
