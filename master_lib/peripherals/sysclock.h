4d568d1ec699baf4533d15ab8d4e87b9b90314f5
Kyle Wuerch <kyle98678@gmail.com>
2019-06-02 12:50:40 -0700

    Move system clock configuration into master_lib

---
+++ master_lib/peripherals/sysclock.h
@@ -0,0 +1,6 @@
+#ifndef __SYSCLOCK_H__
+#define __SYSCLOCK_H__
+
+void SystemClock_Config(void);
+
+#endif
--- olympus/main.c
+++ olympus/main.c
@@ -28,6 +28,7 @@
 #include "doubleBuffer.h"
 #include "submoduleComms.h"
 #include "timerCallback.h"
+#include "sysclock.h"
 
 #include "hephaestus.h"
 #include "janus.h"
@@ -43,8 +44,6 @@
 #include "app_ethernet.h"
 
 #include <stddef.h>
-
-static void SystemClock_Config(void);
 
 struct netif gnetif;
 
@@ -97,90 +96,23 @@ int main(void)
     {
         serviceTxDma();
         serviceCallbackTimer();
-        serviceUSBWrite();
-
-        ethernetif_input(&gnetif);
-        sys_check_timeouts();
 
         consoleProcessBytes();
 
         uint8_t dataIn;
-        while (doubleBuffer_read(&usbReceiveBuffer, &dataIn, 1))
+        if(usingUSB)
         {
-            runCommsFSM((char)dataIn, (void*)0);
+            serviceUSBWrite();
+
+            while (doubleBuffer_read(&usbReceiveBuffer, &dataIn, 1))
+            {
+                runCommsFSM((char)dataIn, NULL);
+            }
+        }
+        else
+        {
+            ethernetif_input(&gnetif);
+            sys_check_timeouts();
         }
     }
-}
-
-
-/** System Clock Configuration
-*/
-static void SystemClock_Config(void)
-{
-
-#ifdef DEV
-    RCC_OscInitTypeDef RCC_OscInitStruct;
-    RCC_ClkInitTypeDef RCC_ClkInitStruct;
-
-    /* Enable HSE Oscillator and activate PLL with HSE as source */
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-    RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-    RCC_OscInitStruct.PLL.PLLM = 8;
-    RCC_OscInitStruct.PLL.PLLN = 240;
-    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-    RCC_OscInitStruct.PLL.PLLQ = 5;
-    HAL_RCC_OscConfig(&RCC_OscInitStruct);
-
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK |
-                            RCC_CLOCKTYPE_SYSCLK |
-                            RCC_CLOCKTYPE_PCLK1|
-                            RCC_CLOCKTYPE_PCLK2;
-
-    /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
-     clocks dividers */
-    RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
-
-    HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3);
-
-    HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
-
-    HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
-
-    HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
-#else
-    RCC_OscInitTypeDef RCC_OscInitStruct;
-    RCC_ClkInitTypeDef RCC_ClkInitStruct;
-    RCC_OscInitTypeDef RCC_OscInitStruct;
-
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI|RCC_OSCILLATORTYPE_HSE;
-    RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
-    RCC_OscInitStruct.LSIState = RCC_LSI_ON;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-    RCC_OscInitStruct.PLL.PLLM = 16;
-    RCC_OscInitStruct.PLL.PLLN = 192;
-    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-    RCC_OscInitStruct.PLL.PLLQ = 5;
-    HAL_RCC_OscConfig(&RCC_OscInitStruct);
-
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_SYSCLK|RCC_CLOCKTYPE_PCLK1
-                              |RCC_CLOCKTYPE_PCLK2;
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
-    HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3);
-
-    HAL_RCC_EnableCSS();
-
-    HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
-
-    HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
-#endif
 }
