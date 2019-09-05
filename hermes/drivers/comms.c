d2bb78a003022d2dc4bc5510e381849874eb3f30
Kyle Wuerch <kyle98678@gmail.com>
2019-09-04 19:54:57 -0700

    Add updated messaging skeleton code for hermes

--- hermes/drivers/comms.c
+++ hermes/drivers/comms.c
@@ -11,18 +11,24 @@
 static uint8_t get_status_callback(uint8_t *data);
 static uint8_t get_aman_callback(uint8_t *data);
 static uint8_t get_speed_callback(uint8_t *data);
+static uint8_t get_pedal_callback(uint8_t *data);
+static uint8_t get_starg_callback(uint8_t *data);
+static uint8_t set_starg_callback(uint8_t *data);
 
-#define AMAN_IDX    ((uint32_t)1u)
-#define SPEED_IDX   ((uint32_t)2u)
+#define GET_AMAN_IDX    ((uint32_t)1u)
+#define GET_SPEED_IDX   ((uint32_t)2u)
+#define GET_PEDAL_IDX   ((uint32_t)3u)
+#define GET_STARG_IDX   ((uint32_t)4u)
+#define SET_STARG_IDX   ((uint32_t)5u)
 
 /** tx or rx DataLengths can be no longer than 253(rx) 253(tx) **/
 msgInfo_t msgResp[NUM_MSGS] = {
     {0u, 0u, get_status_callback}, // 0 Get Status
     {0u, 1u, get_aman_callback},   // 1 Get Automan
     {0u, 0u, get_speed_callback},  // 2 Get Speed
-    {0u, 0u, NULL},                // 3 Unused
-    {0u, 0u, NULL},                // 4 Unused
-    {0u, 0u, NULL},                // 5 Unused
+    {0u, 0u, get_pedal_callback},  // 3 Get Pedal
+    {0u, 2u, get_starg_callback},  // 4 Get Speed Target
+    {2u, 0u, set_starg_callback},  // 5 Set Speed Target
     {0u, 0u, NULL},                // 6 Unused
     {0u, 0u, NULL},                // 7 Unused
     {0u, 0u, NULL},                // 8 Unused
@@ -49,3 +55,16 @@ static uint8_t get_speed_callback(uint8_t *data){
 
     return WR_OK;
 }
+
+static uint8_t get_pedal_callback(uint8_t *data){
+    return WR_OK;
+}
+
+static uint8_t get_starg_callback(uint8_t *data){
+    return WR_OK;
+}
+
+static uint8_t set_starg_callback(uint8_t *data){
+    return WR_OK;
+}
+
---
+++ hermes/peripherals/adc.c
@@ -0,0 +1,126 @@
+#include <string.h>
+
+#include "stm32f0xx_hal.h"
+#include "adc.h"
+
+static volatile uint16_t adcBuffer[ADC_BUFF_SIZE];
+
+uint16_t adcChnAvg[2u][NUM_ADC_CHANNELS];
+int adcChnWriteIdx = 0u;
+int adcChnReadIdx = 1u;
+
+static void ADC1_LL_Init();
+
+void MX_ADC1_Init()
+{
+    ADC1_LL_Init();
+
+    /** ADC Calibration */
+    if((ADC1->CR & ADC_CR_ADEN) != 0u)
+    {
+        ADC1->CR |= ADC_CR_ADDIS;
+    }
+
+    while((ADC1->CR & ADC_CR_ADEN) != 0u);
+
+    ADC1->CFGR1 &= ~ADC_CFGR1_DMAEN;
+    ADC1->CR |= ADC_CR_ADCAL;
+
+    while((ADC1->CR & ADC_CR_ADCAL) != 0u);
+
+    /** ADC Setup
+     * Continuous Mode, Overwrite Data on Overrun, Enable Circular DMA
+     * Channel 2
+     **/
+    ADC1->CFGR1 |= ADC_CFGR1_CONT | ADC_CFGR1_OVRMOD | ADC_CFGR1_DMAEN | ADC_CFGR1_DMACFG;
+    ADC1->CHSELR = ADC_CHSELR_CHSEL2;
+
+    /** DMA Configuration **/
+    DMA1_Channel1->CPAR = (uint32_t)(&(ADC1->DR));
+    DMA1_Channel1->CMAR = (uint32_t)adcBuffer;
+    DMA1_Channel1->CNDTR = ADC_BUFF_SIZE;
+
+    /**
+     * Memory Increment Mode, 16-bit Mem Size, 16-bit Peripheral Size,
+     * Transfer Error and Transfer Complete Interrupt Enable,
+     * Circular Mode
+     */
+    DMA1_Channel1->CCR |= DMA_CCR_MINC | DMA_CCR_MSIZE_0 | DMA_CCR_PSIZE_0 |
+                          DMA_CCR_TEIE | DMA_CCR_TCIE | DMA_CCR_CIRC; 
+    DMA1_Channel1->CCR |= DMA_CCR_EN;
+
+    NVIC_EnableIRQ(DMA1_Channel1_IRQn);
+
+    /** ADC Enable **/
+    if((ADC1 -> ISR & ADC_ISR_ADRDY) != 0u)
+    {
+        ADC1->ISR |= ADC_ISR_ADRDY;
+    }
+
+    ADC1->CR |= ADC_CR_ADEN;
+    while((ADC1->ISR & ADC_ISR_ADRDY) == 0u);
+
+    ADC1->CR |= ADC_CR_ADSTART;
+}
+
+static void ADC1_LL_Init()
+{
+    /** Enable Clock **/
+    RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
+    RCC->CR2 |= RCC_CR2_HSI14ON;
+    while((RCC->CR2 & RCC_CR2_HSI14RDY) == 0u);
+
+    RCC->AHBENR |= RCC_AHBENR_DMA1EN;
+    
+    /** Remap ADC DMA to Channel 1 **/
+    SYSCFG->CFGR1 &= ~SYSCFG_CFGR1_ADC_DMA_RMP;
+
+    /** Enable GPIO **/
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+
+    GPIO_InitTypeDef GPIO_InitStruct;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.Pin = GPIO_PIN_2;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
+
+
+void DMA1_Channel1_IRQHandler()
+{
+    /** Transfer Complete **/
+    if(DMA1->ISR & DMA_ISR_TCIF1)
+    {
+        // Pause Transfers
+        DMA1_Channel1->CCR &= ~DMA_CCR_EN;
+        DMA1->IFCR |= DMA_IFCR_CTCIF1;
+
+        // Clear Data & Calculate Average
+        memset(adcChnAvg[adcChnWriteIdx], 0u, NUM_ADC_CHANNELS * sizeof(adcChnAvg[0][0]));
+
+        int idx;
+        for(idx = 0; idx < ADC_BUFF_SIZE; idx++)
+        {
+            adcChnAvg[adcChnWriteIdx][idx % NUM_ADC_CHANNELS] += adcBuffer[idx];
+        }
+        
+        int chn;
+        for(chn = 0; chn < NUM_ADC_CHANNELS; chn++)
+        {
+            adcChnAvg[adcChnWriteIdx][chn] /= ADC_AVG_NUM;
+        }
+
+        adcChnWriteIdx = (adcChnWriteIdx + 1u) % PING_PONG_SIZE;
+        adcChnReadIdx = (adcChnReadIdx + 1u) % PING_PONG_SIZE;
+
+        // Contiue Transfers
+        DMA1_Channel1->CCR |= DMA_CCR_EN;
+    }
+
+    /** Transfer Error **/
+    if(DMA1->ISR & DMA_ISR_TEIF1)
+    {
+        DMA1->IFCR |= DMA_IFCR_CTEIF1;
+    }
+}
